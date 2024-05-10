93a9ea6ea6445c646e18f0e1ae10b8478ae44751
Belarmino Simão Júnior <belarminosimaojunior@gmail.com>
2024-05-11 00:28:25 +0200

    feat: routes

--- frontend/employee-frontend/src/App.js
+++ frontend/employee-frontend/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FooterComponent from "./components/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
@@ -5,9 +6,16 @@ import ListEmployeeComponent from "./components/ListEmployeeComponent";
 function App() {
   return (
     <div className="App">
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent />
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Routes>
+            <Route index path="/" element={<ListEmployeeComponent />} />
+            <Route path="/employees" element={<ListEmployeeComponent />} />
+          </Routes>
+        </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
 }
